361c36f1f7a868b9957de7a7d7934e065e8996a3
fredegd <124848643+fredegd@users.noreply.github.com>
2024-02-01 17:52:44 +0100

    Delete distribution/EasyEase-1 directory

--- distribution/EasyEase-1/reference/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"easy.ease"}];updateSearchResults();
--- distribution/EasyEase-1/reference/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"easy.ease","l":"EasingMethods"},{"p":"easy.ease","l":"EasyEase"}];updateSearchResults();
